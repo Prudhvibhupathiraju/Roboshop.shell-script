5bba3580814858d764b239041317e50abfda7f75
unknown <prudhvirajubh999@gmail.com>
2024-02-15 16:58:28 +0530

    Mongodb.sh c2

---
+++ Mongodb.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo -e "the script has started executing at $Y $TIMESTAMP $N" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+    echo -e "$2 .... $R FAILED $N"
+    else
+    echo -e "$2 .... $G SUCCESS $N"
+    fi
+}
+
+if [ $id -ne 0 ]
+then
+echo -e "$R ERROR :: you are not root user $N"
+else
+echo -e "You are in $G ROOT $N access"
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "Copying Mongo.repoo file"
+
+dnf install mongodb-org -y &>> $LOGFILE
+
+VALIDATE $? "Installing MongoDB"
+
+systemctl enable mongod &>> $LOGFILE
+
+VALIDATE $? "Enabling MongoDB"
+
+systemctl start mongod &>> $LOGFILE
+
+VALIDATE $? "Starting MongoDB"
+
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>> $LOGFILE
+
+VALIDATE $? "Remote access to MongoDB"
+
+systemctl restart mongod &>> $LOGFILE
+
+VALIDATE $? "Restaring MongoDB"
