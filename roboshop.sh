fa49436a76e080a5d8dc23057154f1c43b1367cf
unknown <prudhvirajubh999@gmail.com>
2024-02-28 15:58:26 +0530

    robohsop.sh c3

--- roboshop.sh
+++ roboshop.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo -e "the script has started executing at  $TIMESTAMP " &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+    echo -e "$2 .... $R FAILED TO CREATE $N"
+    
+    else
+    echo -e "$2 .... $G SUCCESSFULLY CREATED $N"
+    fi
+}
+
 AMI=ami-0f3c7d07486cad139
 SG=sg-034af918babda9baf
 Instances=("MongoDB" "Redis" "MySQL" "RabbitMQ" "Catalogue" "Cart" "User" "Shipping" "Payments" "Dispatch" "Web")
@@ -13,5 +33,6 @@ Instance_type="t3.small"
 else
 Instance_type="t2.micro"
 fi
-aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $Instance_type --security-group-ids sg-034af918babda9baf --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $Instance_type --security-group-ids sg-034af918babda9baf --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" &>> $LOGFILE
+VALIDATE $? "Instance $i"
 done
