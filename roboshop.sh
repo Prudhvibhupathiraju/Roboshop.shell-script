a2d104cc18b04cc8ca22a7765f784e74f3c66bd8
unknown <prudhvirajubh999@gmail.com>
2024-02-28 16:36:37 +0530

    robohsop.sh c4

--- roboshop.sh
+++ roboshop.sh
@@ -1,28 +1,10 @@
 #!/bin/bash
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-
-TIMESTAMP=$(date +%F-%H-%M-%S)
-LOGFILE="/tmp/$0-$TIMESTAMP.log"
-
-echo -e "the script has started executing at  $TIMESTAMP " &>> $LOGFILE
-
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then
-    echo -e "$2 .... $R FAILED TO CREATE $N"
-    
-    else
-    echo -e "$2 .... $G SUCCESSFULLY CREATED $N"
-    fi
-}
-
 AMI=ami-0f3c7d07486cad139
 SG=sg-034af918babda9baf
 Instances=("MongoDB" "Redis" "MySQL" "RabbitMQ" "Catalogue" "Cart" "User" "Shipping" "Payments" "Dispatch" "Web")
+ZONE_ID=Z04351902WY9G4CTL518C
+DOMAIN_NAME=bhpr.online
 
 for i in ${Instances[@]}
 do
@@ -33,6 +15,24 @@ Instance_type="t3.small"
 else
 Instance_type="t2.micro"
 fi
-aws ec2 run-instances --image-id ami-0f3c7d07486cad139 --instance-type $Instance_type --security-group-ids sg-034af918babda9baf --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" &>> $LOGFILE
-VALIDATE $? "Instance $i"
+IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $Instance_type --security-group-ids $SG --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text) 
+echo "$i : $IP_ADDRESS"
+aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch "
+  {
+    "Comment": "Testing creating a record set"
+    ,"Changes": [{
+      "Action"              : "CREATE"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'" $i "'$DOMAIN_NAME"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "$IP_ADDRESS"
+        }]
+      }
+    }]
+  }
+  "
 done
